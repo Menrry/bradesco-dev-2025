063b19ec28739b8ac3c0084a9bdea4aedf7a96e6
Menrry <msantanalacruz@gmail.com>
2025-05-03 19:19:37 -0300

    Primer commit

---
+++ bradesco-dev-2025/src/test/java/me/menrry/bradesco_dev_2025/Application.java
@@ -0,0 +1,13 @@
+package me.menrry.bradesco_dev_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
